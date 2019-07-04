cc41ac4d24476ddbd703ac8ac6af1328a2b0aa4b
Moises Campos Zepeda <moises.campos.zepeda@hpe.com>
2019-07-03 21:13:13 -0600

    mux.v con nombres de las señales a utlizar

--- muxes/mux/mux.v
+++ muxes/mux/mux.v
@@ -5,11 +5,11 @@ module mux #(
     )(
     //input                       selector,
     input                       clk,
-    input                       pop_delay_VC0,      //funciona como un enable
-    input                       pop_delay_VC1,      //funciona como un enable
-    input       [DATA_SIZE-1:0] data_mux0,
-    input       [DATA_SIZE-1:0] data_mux1,
-    output reg  [DATA_SIZE-1:0] data_demux
+    input                       pop_delay_vc0,      //funciona como un enable
+    input                       pop_delay_vc1,      //funciona como un enable
+    input       [DATA_SIZE-1:0] data_mux_0,
+    input       [DATA_SIZE-1:0] data_mux_1,
+    output reg  [DATA_SIZE-1:0] data_demux_
 );
     reg         [DATA_SIZE-1:0] reg_VC0, reg_VC1;
 
@@ -18,7 +18,9 @@ module mux #(
         //Seleccion interna de valids
         reg_VC0 = pop_delay_VC0 ? data_mux0 : 'b0;  
         reg_VC1 = pop_delay_VC1 ? data_mux1 : 'b0; 
-        data_demux =  pop_delay_VC0 ? reg_VC0 : reg_VC1;
     end
-
+    
+    always@( posedge clk)begin
+        data_demux <=  pop_delay_VC0 ? reg_VC0 : reg_VC1;
+    end
 endmodule
