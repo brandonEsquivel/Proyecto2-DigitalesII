dbc5b38659cb17139c7aa2084cb47b0a6a9e4ee9
giancamarin <gianca.1996mh@gmail.com>
2019-07-03 19:40:01 -0600

    Modificacion de arquitectura con todas las entradas y salidas

--- arquitectura/arqui.v
+++ arquitectura/arqui.v
@@ -18,120 +18,58 @@
 module arqui#(
     parameter MAIN_QUEUE_SIZE = 4,        //Cantiad de filas del main fifo
     parameter OUTPUT_SIZE = 6,
-    parameter DATA_SIZE = 3             //cantidad de bits de entrada
+    parameter DATA_SIZE = 6             //cantidad de bits de entrada
 )(
     input                               clk,
     input                               reset_L,
-    input       [MAIN_QUEUE_SIZE-1:0]   data_in,            //datos para hacerle push 
+    input       [DATA_SIZE-1:0]   		data_in,            //datos para hacerle push 
+    input 								push_M,
+    input 								pop_d0,
+    input 								pop_d1,
+    input [1:0]							afMF_i,				// almost full Main FIFO
+    input [1:0]							aeMF_i,				// almost empty Main FIFO
+    input [3:0]							afVC_i,				// almost full VC FIFO
+    input [3:0]							aeVC_i,				// almost empty VC FIFO
+    input [1:0]							afDF_i,				// almost full D FIFO
+    input [1:0]							aeDF_i,				// almost empty D FIFO
+    output reg 							error_out,			
+    output reg 							active_out,			
+    output reg 							idle_out,			
     output reg  [OUTPUT_SIZE-1:0]       data_out0,
     output reg  [OUTPUT_SIZE-1:0]       data_out1
-
 );
 
-
-    fsm fsm0(/*AUTOINST*/
-	     // Outputs
-	     .error_out_cond		(error_out_cond[4:0]),
-	     .active_out_cond		(active_out_cond),
-	     .idle_out_cond		(idle_out_cond),
-	     // Inputs
-	     .clk			(clk),
-	     .reset_L			(reset_L),
-	     .init			(init),
-	     .FIFO_errors		(FIFO_errors[4:0]),
-	     .FIFO_empties		(FIFO_empties[4:0]),
-	     .afMFs			(afMFs[2:0]),
-	     .aeMFs			(aeMFs[2:0]),
-	     .afVCs			(afVCs[2:0]),
-	     .aeVCs			(aeVCs[2:0]),
-	     .afDs			(afDs[2:0]),
-	     .aeDs			(aeDs[2:0]));
+    fsm fsm0(/*AUTOINST*/);
 
     fifo Main_FIFO#(
         DATA_SIZE = 4,
         MAIN_QUEUE_SIZE = 6
-    )(/*AUTOINST*/
-      // Outputs
-      .almost_full_cond			(almost_full_cond),
-      .almost_empty_cond		(almost_empty_cond),
-      .fifo_full_cond			(fifo_full_cond),
-      .fifo_empty_cond			(fifo_empty_cond),
-      .data_count_cond			(data_count_cond[MAIN_QUEUE_SIZE-1:0]),
-      .buffer_out_cond			(buffer_out_cond[MAIN_QUEUE_SIZE-1:0]),
-      .error_cond			(error_cond),
-      // Inputs
-      .clk				(clk),
-      .reset_L				(reset_L),
-      .read				(read),
-      .write				(write),
-      .buff_in				(buff_in[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_full			(umb_almost_full[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_empty			(umb_almost_empty[MAIN_QUEUE_SIZE-1:0]));
+    )(/*AUTOINST*/);
 
 
     fifo vc0 #(
         DATA_SIZE = 16,
         MAIN_QUEUE_SIZE = 6
-    )(/*AUTOINST*/
-      // Outputs
-      .almost_full_cond			(almost_full_cond),
-      .almost_empty_cond		(almost_empty_cond),
-      .fifo_full_cond			(fifo_full_cond),
-      .fifo_empty_cond			(fifo_empty_cond),
-      .data_count_cond			(data_count_cond[MAIN_QUEUE_SIZE-1:0]),
-      .buffer_out_cond			(buffer_out_cond[MAIN_QUEUE_SIZE-1:0]),
-      .error_cond			(error_cond),
-      // Inputs
-      .clk				(clk),
-      .reset_L				(reset_L),
-      .read				(read),
-      .write				(write),
-      .buff_in				(buff_in[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_full			(umb_almost_full[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_empty			(umb_almost_empty[MAIN_QUEUE_SIZE-1:0]));
+    )(/*AUTOINST*/);
 
 
     fifo vc1 #(
         DATA_SIZE = 16
         MAIN_QUEUE_SIZE = 6
-    )(/*AUTOINST*/
-      // Outputs
-      .almost_full_cond			(almost_full_cond),
-      .almost_empty_cond		(almost_empty_cond),
-      .fifo_full_cond			(fifo_full_cond),
-      .fifo_empty_cond			(fifo_empty_cond),
-      .data_count_cond			(data_count_cond[MAIN_QUEUE_SIZE-1:0]),
-      .buffer_out_cond			(buffer_out_cond[MAIN_QUEUE_SIZE-1:0]),
-      .error_cond			(error_cond),
-      // Inputs
-      .clk				(clk),
-      .reset_L				(reset_L),
-      .read				(read),
-      .write				(write),
-      .buff_in				(buff_in[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_full			(umb_almost_full[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_empty			(umb_almost_empty[MAIN_QUEUE_SIZE-1:0]));
+    )(/*AUTOINST*/);
 
 
     fifo d0 #(
         DATA_SIZE = 4,
         MAIN_QUEUE_SIZE = 6
-    )(/*AUTOINST*/
-      // Outputs
-      .almost_full_cond			(almost_full_cond),
-      .almost_empty_cond		(almost_empty_cond),
-      .fifo_full_cond			(fifo_full_cond),
-      .fifo_empty_cond			(fifo_empty_cond),
-      .data_count_cond			(data_count_cond[MAIN_QUEUE_SIZE-1:0]),
-      .buffer_out_cond			(buffer_out_cond[MAIN_QUEUE_SIZE-1:0]),
-      .error_cond			(error_cond),
-      // Inputs
-      .clk				(clk),
-      .reset_L				(reset_L),
-      .read				(read),
-      .write				(write),
-      .buff_in				(buff_in[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_full			(umb_almost_full[MAIN_QUEUE_SIZE-1:0]),
-      .umb_almost_empty			(umb_almost_empty[MAIN_QUEUE_SIZE-1:0]));
+    )(/*AUTOINST*/);
+
+
+    fifo d1 #(
+        DATA_SIZE = 4,
+        MAIN_QUEUE_SIZE = 6
+    )(/*AUTOINST*/);
+
+
 
 endmodule
