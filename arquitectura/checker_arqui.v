f739dd9e3cef2e0bf85533fea26ae8f506484471
moisotico <moiso.camposcr@gmail.com>
2019-07-10 01:16:52 -0600

    checker y probador

--- arquitectura/checker_arqui.v
+++ arquitectura/checker_arqui.v
@@ -6,29 +6,59 @@
 *@brief Checker para comparar comportamiento conductual con el estructural
 */
 
-module checker_arqui(
-    output reg      arqui_checks_out,        //salida que indica que ambos datos de la salida son iguales
-    input          clk,
-    input          reset_L,
-    input   [4:0]  salida_arqui_c,
-    input   [4:0]   salida_arqui_e
+module checker_arqui (
+    output reg     arqui_checks_out,        //salida que indica que ambos datos de la salida son iguales
+    input       clk,
+    input       reset_L,
+    input [3:0] salida_arqui0_c,
+    input [3:0] salida_arqui0_e,
+    input [3:0] salida_arqui1_c,
+    input [3:0] salida_arqui1_e,
+    input       salida_arqui2_c,
+    input       salida_arqui2_e,
+    input       salida_arqui3_c,
+    input       salida_arqui3_e,
+    input       salida_arqui4_c,
+    input       salida_arqui4_e
 );
-    reg     out_c, out_e;                   //flip-flops
-    reg     onflag, resetflag;                         //checks if message has been sent   
+     //flip-flops
+    reg out0_c, out0_e, out1_c, out1_e, out2_c, out2_e, out3_c, out3_e, out4_c, out4_e; 
+    
+    reg onflag, resetflag;                         //checks if message has been sent   
     always@( * ) begin
         if( !reset_L ) begin
-            out_c  =   'b0;
-            out_e  =   'b0;
-            onflag =   'b0;
-            arqui_checks_out <= 1;
+            out0_c =   'b0;
+            out0_e =   'b0;
+            out1_c =   'b0;
+            out1_e =   'b0;
+            out2_c =   'b0;
+            out2_e =   'b0;
+            out3_c =   'b0;
+            out3_e =   'b0;
+            out4_c =   'b0;
+            out4_e =   'b0;
+            onflag  =   'b0;
+            arqui_checks_out = 1;
         end else begin      
-            out_c = salida_arqui_c;
-            out_e = salida_arqui_e;
+            out0_c = salida_arqui0_c;
+            out0_e = salida_arqui0_e;
+            out1_c = salida_arqui1_c;
+            out1_e = salida_arqui1_e;
+            out2_c = salida_arqui2_c;
+            out2_e = salida_arqui2_e;
+            out3_c = salida_arqui3_c;
+            out3_e = salida_arqui3_e;
+            out4_c = salida_arqui4_c;
+            out4_e = salida_arqui4_e;
            
-            if( reset_L == 1 ) begin        //comparacion en los flancos del reloj
-                if( out_c == out_e ) begin
+            if( reset_L ) begin        //comparacion en los flancos del reloj
+                if(     (out0_c == out0_e)
+                    &&  (out1_c == out1_e)
+                    &&  (out2_c == out2_e)
+                    &&  (out3_c == out3_e)
+                    &&  (out4_c == out4_e)  )begin
                     arqui_checks_out = 1;
-                    resetflag =1; 
+                    resetflag = 1; 
                 end else begin
                     arqui_checks_out = 'b0;
                     if ( ( !onflag )  && ( resetflag == 1 ) ) begin
--- arquitectura/probador.v
+++ arquitectura/probador.v
@@ -1,48 +1,58 @@
 /*
  *Universidad de Costa Rica - Escuela de Ingenieria Electrica
  *Proyecto #1 - IE-0523 - modulo probador del arqui
- *@author Giancarlo Marin H.
+ *@author Giancarlo Marin H., Moises Campos Z. 
  *@date   02/07/2019
  *@brief  Modulo que genera las señales y monitoriza las salidas del modulo arqui conductual y estructural sintetizado
 */
 
 `include "checker_arqui.v"
-    module probador #(   
-        parameter DATA_SIZE = 4, 
+module probador #(   
+        parameter DATA_SIZE = 6, 
         parameter OUTPUT_SIZE = 4
     )(
-        output reg [DATA_SIZE-1:0]    data_in,            //datos para hacerle push 
-        output reg                    push_M,
-        output reg                    pop_d0,
-        output reg                    pop_d1,
-        output reg [1:0]              afMF_i,            // almost full Main FIFO
-        output reg [1:0]              aeMF_i,            // almost empty Main FIFO
-        output reg [3:0]              afVC_i,                 // almost full VC FIFO
-        output reg [3:0]              aeVC_i,            // almost empty VC FIFO
-        output reg [1:0]              afDF_i,            // almost full D FIFO
-        output reg [1:0]              aeDF_i,            // almost empty D FIFO
-        input                         error_out,
-        input                         active_out,
-        input                         idle_out,
-    
-    
-    input       [OUTPUT_SIZE-1:0]	    data_out0_c,	// monitor de salida 0 de datos cond
-	input       [OUTPUT_SIZE-1:0]	    data_out0_e	    // monitor de salida 0 de datos estr
-    input       [OUTPUT_SIZE-1:0]	    data_out1_c,	// monitor de salida 0 de datos cond
-	input       [OUTPUT_SIZE-1:0]	    data_out1_e	    // monitor de salida 0 de datos estr
-	);
+        output reg                      clk,
+        output reg                      reset_L,
+        output reg [DATA_SIZE-1:0]      data_in,            //datos para hacerle push 
+        output reg                      push_main,
+        output reg                      pop_d0,
+        output reg                      pop_d1,
+        output reg                      init,
+        
+        output reg                      fifo_pause_main,
+        output reg                      fifo_empty_d0,
+        output reg                      fifo_empty_d1,
+
+        output reg [1:0]                afMF_i,            // almost full Main FIFO
+        output reg [1:0]                aeMF_i,            // almost empty Main FIFO
+        output reg [3:0]                afVC_i,                 // almost full VC FIFO
+        output reg [3:0]                aeVC_i,            // almost empty VC FIFO
+        output reg [1:0]                afDF_i,            // almost full D FIFO
+        output reg [1:0]                aeDF_i,            // almost empty D FIFO
+        
+        input                           error_out_c,
+        input                           error_out_e,
+
+        input                           active_out_c,
+        input                           active_out_e,
+
+        input                           idle_out_c,
+        input                           idle_out_e, 
+        
+        input   [DATA_SIZE-1:0]         data_out0_c,	// monitor de salida 0 de datos cond
+        input   [DATA_SIZE-1:0]	        data_out0_e,	    // monitor de salida 0 de datos estr
+        
+        input   [DATA_SIZE-1:0]	        data_out1_c,	// monitor de salida 0 de datos cond
+        input   [DATA_SIZE-1:0]	        data_out1_e	    // monitor de salida 0 de datos estr
+);
 
 /*AUTOWIRE*/
 // Beginning of automatic wires (for undeclared instantiated-module outputs)
 wire			arqui_checks_out;	// From ch0 of checker_arqui.v
 // End of automatics
-/*AUTOREGINPUT*/
-// Beginning of automatic reg inputs (for undeclared instantiated-module inputs)
-reg			clk;			// To ch0 of checker_arqui.v
-reg			reset_L;		// To ch0 of checker_arqui.v
-reg [4:0]		salida_arqui_c;		// To ch0 of checker_arqui.v
-reg [4:0]		salida_arqui_e;		// To ch0 of checker_arqui.v
-// End of automatics
+
+//seleccion 
+reg select;
 
     checker_arqui ch0(/*autoinst*/
 		      // Outputs
@@ -50,8 +60,16 @@ reg [4:0]		salida_arqui_e;		// To ch0 of checker_arqui.v
 		      // Inputs
 		      .clk		(clk),
 		      .reset_L		(reset_L),
-		      .salida_arqui_c	(salida_arqui_c[4:0]),
-		      .salida_arqui_e	(salida_arqui_e[4:0]));
+		      .salida_arqui0_c	(data_out0_c[5:0]),
+		      .salida_arqui0_e	(data_out0_e[5:0]),
+		      .salida_arqui1_c	(data_out1_c[5:0]),
+		      .salida_arqui1_e	(data_out1_e[5:0]),
+		      .salida_arqui2_c	(error_out_c),
+		      .salida_arqui2_e	(error_out_e),
+		      .salida_arqui3_c	(active_out_c),
+		      .salida_arqui3_e	(active_out_e),
+		      .salida_arqui4_c	(idle_out_c),
+		      .salida_arqui4_e	(idle_out_e));
 
 	initial begin
 		$dumpfile("arqui.vcd");		// archivo "dump"
@@ -59,81 +77,50 @@ reg [4:0]		salida_arqui_e;		// To ch0 of checker_arqui.v
 		// Mensajes en consola 
 		//$display ("\tclk\tdata_VC0\tcheck_out0 check_out1");
 		//$monitor($time,"\t%b\t%b\t%x %b\t%x %b\t%b %b", clk,  data_VC0, check_out0,check_out1);
-		// Pruebas
-        /*
-        pop_delay_VC0 <= 'b0;
-		data_VC0    <= 'b0; 					
-		data_VC1    <= 'b0; 					
-		@(posedge clk);
-		// Sincroniza
-		@(posedge clk);
-        reset_L <= 'b0;
-        selector <= 0;
-		data_VC0 <= 'hf;
-		data_VC1 <= 'h5;
-        pop_delay_VC0 <= 1;
-		repeat(3) begin
+		// Prueba#0: valores iniciales
+        
+            reset_L     <= 'b0;
+            data_in     <= 'b0; 					
+            init        <= 'b0; 					
+	        pop_d0      <= 'b0;
+            pop_d1      <= 'b0;    
+            push_main   <= 'b0;
+
+		// Prueba#1: reset activado
+        @(posedge clk);
+            reset_L <= 1;
+            data_in <= 'b0;
+        @ (posedge clk)
+            data_in <= 'h0f;
+            push_main <= 0;
+            
+        // Prueba #2: umbrales para la maquina de estados
 			@(posedge clk) begin
-                reset_L <= 1;
-				data_VC0 <= data_VC0 - 'h1;
-				data_VC1 <= data_VC1 - 'h1;
+				data_in <= data_in - 1;
+                afMF_i  <=  3;
+                aeMF_i  <=  1;
+                afVC_i  <=  14;
+                aeVC_i  <=  3;
+                afDF_i  <=  1;
+                aeDF_i  <=  3;
+
+				push_main <= 1;
 			end
-		end
-		// Prueba #3: Espera
-		@(posedge clk) begin
-            selector <= 1;
-			pop_delay_VC0 <= 0;
-		end
-		@(posedge clk);
-		// Prueba #4: Continua la entrada de dato_in por dos ciclos mas
-        selector <= 0;
-		@(posedge clk) begin
-			pop_delay_VC0 <= 1;
-			data_VC0 <= 'h3;
-			data_VC1 <= 'h5;
-		end
-		@(posedge clk) begin
-			data_VC1 <= 'h4;
-			data_VC0 <= 'h6;
-		end
-		@(posedge clk) begin
-			pop_delay_VC0 <= 0;
-		end
-		@(posedge clk);
-		@(posedge clk);
-		// Prueba #5: Prueba del funcionamiento de selector interno con entrada de mas datos
-		@(posedge clk) begin
-            selector <= 1;
-			pop_delay_VC0 <= 1;
-			data_VC1 <= 'ha;
-		end
-		@(posedge clk) begin
-			data_VC0 <= 'h9;
-		end
-		@(posedge clk) begin
-			pop_delay_VC0 <= 0;
-		end
-		@(posedge clk);
-		@(posedge clk);
-		// Prueba #6: Ultima entrada de datos
-		@(posedge clk) begin
-			pop_delay_VC0 <= 1;
-			data_VC1 <= 'h7;
-		end
-		@(posedge clk) begin
-			data_VC0 <= 'h8;
-		end
-		@(posedge clk) begin
-			pop_delay_VC0 <= 0;
-		end
-		@(posedge clk);
-		@(posedge clk);
-		// Prueba 7: Reset alto y termina de almacenar señales
-		@(posedge clk);
-        */
-		#10 $finish;	
+
+        #10;
+        $finish;	
 	end
-	// Generador del clk
-	initial	clk <= 0;				// Valor inicial del clk
-	always #10 clk <= ~clk;			// toggle cada 10ns
+	
+    // Generador del clk
+	initial clk <= 0;				// Valor inicial del clk
+	always #2 clk <= ~clk;			// toggle cada 10ns
+    
+    always@(*) begin
+        if(!fifo_pause_main) begin
+            push_main = 1;
+        end else
+            push_main = 0;
+    end
+
+
 endmodule
